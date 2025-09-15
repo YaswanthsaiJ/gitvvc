936f2ab4881ab1097e35b9dfd186f7e9bfe94bcd
YaswanthsaiJ <151212177+YaswanthsaiJ@users.noreply.github.com>
2025-09-15 23:01:52 +0530

    Create COH

---
+++ COH.java
@@ -0,0 +1,11 @@
+ class COH{
+    public static void main(String[] args) {
+        int num = 4;  
+        int cube = num * num * num;   
+        double half = cube / 2.0;     
+
+       
+        System.out.println("Cube: " + cube);
+        System.out.println("Half of Cube: " + half);
+    }
+}
